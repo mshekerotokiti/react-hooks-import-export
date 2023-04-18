0a07d86b39121f4d9165df18189364488aa86001
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2023-04-18 13:14:33 +0300

    completed code along

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,21 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import {trees, wildlife} from "./parks/RockyMountain";
 
+console.log(trees);
+wildlife();
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+  <div>
+   <h1>Colorado State Parks!</h1>
+   <MesaVerde/>
+   
+   
+
+  </div>
+  )
 }
+export default ColoradoStateParks
